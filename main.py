83f3ef3934d418182aacdaa285ca3cee6e3fd025
Artem <69763892+artem9677@users.noreply.github.com>
2024-10-20 17:25:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-string = str(input("Введите строку: "))
+string = str(input("Введите строку: ")) # ввод строки 
 
-print(string.replace("а","о"))
+string = string.lower() # приведение к нижнему регистру
+
+print(string.replace("а","о")) # замена букв и вывод
